9160d1cb533956127aca51cf1b4175d1d8551c88
M-JANDAR12 <74101571+M-JANDAR12@users.noreply.github.com>
2021-01-27 20:35:33 +0200

    matches function added
    matches function added that gets matches results for the current teams' stats

--- scraper.py
+++ scraper.py
@@ -4,7 +4,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 from selenium.webdriver.chrome.options import Options
+from datetime import datetime, timedelta
+import os
 
+
+WINDOW_SIZE = "1280,720"
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -41,8 +45,7 @@ def summery():
 			for item in items:
 				l1.append(item.text)
 			l2.append(l1)
-		# nextbutton = driver.find_element_by_xpath('/html/body/div[4]/div[3]/div[3]/div[2]/div[4]/div/dl[2]/dd[3]/a')
-		# nextbutton.click()
+
 		button = driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[3]/div[2]/div[4]/div/dl[2]/dd[3]/a")
 		driver.execute_script("arguments[0].click();", button)
 
@@ -112,6 +115,59 @@ def offensive():
 	df.drop('Tournament',axis='columns',inplace=True)
 	return df
 
+def matches():
+	url = 'https://www.whoscored.com/LiveScores'
+	driver.get(url)
+	Tournaments = ["Spain - LaLiga","Italy - Serie A","Germany - 1. Bundesliga","England - Premier League","France - Ligue 1"]
+
+
+
+	d_button = driver.find_element_by_xpath("/html/body/div[4]/div[3]/div[1]/div[3]/dl[1]/dd[1]/div/a[1]/span")
+	driver.execute_script("arguments[0].click();", d_button)
+
+	time.sleep(1)
+
+	legues = driver.find_elements_by_xpath('//*[@class="divtable-row group ls-1 stream-is-available"]')
+
+	def res_split(res):
+		for i in range(len(res)):
+
+			if res[i] == ':':
+				home = int(res[0:i-1])
+				away = int(res[i+1:])
+				break
+		return home, away
+
+
+
+
+	data_columns = ['Home','Away','home goals','away goals']
+	data = []
+	for item in legues:
+
+		ID = item.get_attribute("id")
+		team = item.find_element_by_class_name('tournament-link').text
+		if team in Tournaments:
+			# print('////',team,'////')
+			matches = driver.find_elements_by_xpath('//*[@data-group-id="{}"]'.format(int(ID[1:])))
+			for matche in matches:
+				home = matche.find_element_by_class_name('home')
+				away = matche.find_element_by_class_name('away')
+				result = matche.find_element_by_class_name('result')
+				home_r, away_r = res_split(result.text)
+				data.append([home.text,away.text,home_r,away_r])
+				# print(home.text , '/////' , away.text)
+				# print(result.text)
+
+	df = pd.DataFrame(data,columns = data_columns)
+	return df
+
+
+
+
+
+
+
 df1 = summery()
 time.sleep(2)
 df2 = defensive()
@@ -121,7 +177,34 @@ df3 = offensive()
 dfs = [df1, df2, df3]
 dfs = [df.set_index('Team') for df in dfs]
 df = dfs[0].join(dfs[1:])
+stop = False
+
+dfm = matches()
+
+date = datetime.date(datetime.now())
+yesterday = date - timedelta(1)
+yesterday = str(yesterday)
+date = str(date)
+#os.mkdir(yesterday)
+try:
+	os.mkdir("AI_data")
+except :
+	FileExistsError
 
 
-df.to_csv(r'total_data.csv',index = True)
-driver.close()
+try:
+	os.mkdir('AI_data/{}'.format(yesterday))
+except:
+	FileExistsError
+
+dfm.to_csv('AI_data/{}/matches.csv'.format(yesterday),index = False)
+
+
+try:
+	os.mkdir('AI_data/{}'.format(date))
+except:
+	FileExistsError
+	stop = True
+
+if not stop:
+	df.to_csv('AI_data/{}/stats.csv'.format(date),index = True)
